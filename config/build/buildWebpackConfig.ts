11b106a0294611a2bd253e56a0c6574caeebb064
Timur Salahudinov <tima.hardcore@gmail.com>
2023-12-04 14:24:16 +0600

    add babel, add extract plugin

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,14 +1,17 @@
-import { buildDevServer } from "./buildDevServer";
-import { buildLoaders } from "./buildLoaders";
-import { buildPlugins } from "./buildPlugins";
-import { buildResolvers } from "./buildResolvers";
-import { BuildOptions } from "./types/config";
-import webpack from "webpack";
+import { buildDevServer } from './buildDevServer';
+import { buildLoaders } from './buildLoaders';
+import { buildPlugins } from './buildPlugins';
+import { buildResolvers } from './buildResolvers';
+import { BuildOptions } from './types/config';
+import webpack from 'webpack';
 
-export function buildWebpackConfig (options: BuildOptions): webpack.Configuration{
-    const {paths, mode, isDev}=options;
+export function buildWebpackConfig(
+  options: BuildOptions
+): webpack.Configuration {
+  const { paths, mode, isDev } = options;
 
-    return {mode,
+  return {
+    mode,
     entry: paths.entry,
     output: {
       filename: '[name].[contenthash].js',
@@ -19,8 +22,8 @@ export function buildWebpackConfig (options: BuildOptions): webpack.Configuratio
     module: {
       rules: buildLoaders(options),
     },
-    resolve: buildResolvers(),
-    devtool: isDev ? "inline-source-map" : undefined,
+    resolve: buildResolvers(options),
+    devtool: isDev ? 'inline-source-map' : undefined,
     devServer: isDev ? buildDevServer(options) : undefined,
-}
+  };
 }
